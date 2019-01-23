c322069c93704fae68989bc75fc5cf4dce258ce1
quintg1991@gmail.com <fglover@una.edu>
2019-01-23 00:53:17 -0600

    Added comments

--- returningJson/js/app.js
+++ returningJson/js/app.js
@@ -1,9 +1,15 @@
 (
+  // Define a basic function
   function()
   {
 
+    // The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
+    // With strict mode, you can not, for example, use undeclared variables.
     'use strict';
 
+    // Declare our Angular module (ng-app) and name it testApp.
+    // ngRoute is a module that allows for dynamic view that can be added at a later date if we want
+    // to go in that direction
     angular.module
     (
       'testApp', 
@@ -13,5 +19,5 @@
     );
   }
 )
-(
-);
+// Make this function call itself
+();
