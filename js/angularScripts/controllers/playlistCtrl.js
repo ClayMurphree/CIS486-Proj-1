72170b922899ebb2229624fa86b9345316fb9f6d
clay murphree <clay@tropo.org>
2019-04-16 14:24:13 -0500

    moving JS commands to the CTRL

--- js/angularScripts/controllers/playlistCtrl.js
+++ js/angularScripts/controllers/playlistCtrl.js
@@ -6,38 +6,50 @@
 
     angular.module('myApp').controller('playlistCtrl', playlistCtrl);
 
-
+	console.log("Hello Clay");
     function playlistCtrl($scope)
     {
 		 $scope.video = 
 		 [
 			{
 				"title": "Queen - We Are The Champions (Official Video)",
-				"link": "https://youtu.be/04854XqcfCY"
+				"link": "https://www.youtube.com/embed/04854XqcfCY"
 			},
 			{
 				"title": "Queen - We Are The Champions (Lyrics)",
-				"link": "https://youtu.be/Jmd4OLzhQw0"
+				"link": "https://www.youtube.com/embed/Jmd4OLzhQw0"
 			},
 			{
 				"title": "Queen - We Are The Champions (Live)",
-				"link": "https://youtu.be/KXw8CRapg7k"
+				"link": "https://www.youtube.com/embed/KXw8CRapg7k"
 			},
 			{
 				"title": "GLEE - We Are The Champions (Full Performance) HD)",
-				"link": "https://youtu.be/bGMo2SwWK4Q"
+				"link": "https://www.youtube.com/embed/bGMo2SwWK4Q"
 			},
 			{
 				"title": "Jimmy Fallon, The Roots, and Music Superstars Sing 'We Are The Champions' (A Cappella)",
-				"link": "https://youtu.be/KHHqPTQDIlo"
+				"link": "https://www.youtube.com/embed/KHHqPTQDIlo"
 			},
 			{
 				"title": "We are the Champions - Green Day cover",
 				"link": "https://www.youtube.com/embed/DYheOpS03bk" 
 			}
 		]
+		console.log("We still working down here?");
+		var min = 0;
+		var max = $scope.video.length;
+		var num = Math.floor(Math.random() * (max - min + 1)) + min;
+		
+		var x = $scope.video[num].link;
+		
+		$scope.setVideo = function (x) {
+			$scope.currentVideo = $scope.video[num];
+			x = $sce.trustAsResourceUrl($scope.currentProject.url);	
+			console.log(x);
+		}
+		console.log(x);
 	}
-		 
   }
   )
  ();
